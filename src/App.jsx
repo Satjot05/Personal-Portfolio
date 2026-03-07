194f5996b57c1dc96e9beaac6e142e062b9a65a5
Satjot05 <sainisatjot005@gmail.com>
2026-03-07 09:33:41 +0530

    changes are made here

--- src/App.jsx
+++ src/App.jsx
@@ -372,7 +372,7 @@ function Hero() {
   return (
     <section
       id="hero"
-      className="relative min-h-screen flex flex-col items-center justify-center text-center px-6 overflow-hidden"
+      className="relative min-h-screen flex flex-col items-center justify-center text-center px-6 pt-20 overflow-hidden"
     >
       <GridBackground />
 
